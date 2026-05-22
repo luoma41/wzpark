0434afd0f064d3e219aeca52cb8f56ce096e05ef
wz park dev <dev@wzpark.com>
2026-05-22 14:42:47 +0800

    feat: add map data aggregation API

---
+++ api/map-data.js
@@ -0,0 +1,34 @@
+const { getDb } = require('../lib/db');
+const { sendSuccess } = require('../lib/utils');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'GET') return res.status(405).end();
+
+  const db = await getDb();
+  const photos = db.collection('photos');
+
+  const pipeline = [
+    { $match: { lat: { $exists: true }, lng: { $exists: true } } },
+    {
+      $group: {
+        _id: { city: '$city', province: '$province' },
+        lat: { $first: '$lat' },
+        lng: { $first: '$lng' },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        city: '$_id.city',
+        province: '$_id.province',
+        lat: 1,
+        lng: 1,
+        count: 1,
+      },
+    },
+  ];
+
+  const data = await photos.aggregate(pipeline).toArray();
+  sendSuccess(res, data);
+};
