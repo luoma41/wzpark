74c43b31cd9ed8f9958b4d92976bfedd33e60fac
luoma41 <zzn4177@qq.com>
2026-05-23 19:26:59 +0800

    fix: simplify cleanup-orphans to static response to test Vercel cache

--- api/cleanup-orphans.js
+++ api/cleanup-orphans.js
@@ -1,33 +1,14 @@
-const { getDb } = require('../../lib/db');
-const { sendSuccess, sendError } = require('../../lib/utils');
-
 module.exports = async (req, res) => {
-  if (req.method !== 'POST') return sendError(res, 405, 'Method not allowed');
-
-  try {
-    const db = await getDb();
-    const photos = db.collection('photos');
-    const photoDocs = await photos.find({}, { projection: { cosUrl: 1 } }).toArray();
-    const usedKeys = photoDocs.map(p => {
-      try {
-        const url = new URL(p.cosUrl);
-        return decodeURIComponent(url.pathname.slice(1));
-      } catch {
-        return null;
-      }
-    }).filter(Boolean);
-
-    return sendSuccess(res, {
-      version: '4.0',
-      totalDbFiles: usedKeys.length,
+  res.status(200).json({
+    success: true,
+    data: {
+      version: '5.0',
+      totalDbFiles: 0,
       totalCosFiles: 0,
       orphansFound: 0,
       orphanKeys: [],
       deleted: 0,
-      message: 'COS cleanup temporarily disabled. Total DB files: ' + usedKeys.length,
-    });
-  } catch (err) {
-    console.error('Cleanup error:', err);
-    return sendError(res, 500, err.message + ' | STACK: ' + (err.stack || 'none'));
-  }
+      message: 'COS cleanup temporarily disabled. Please clean orphan files manually from Tencent Cloud console.'
+    }
+  });
 };
